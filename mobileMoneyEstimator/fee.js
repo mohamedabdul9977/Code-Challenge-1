6b630b72eaaf847f16b0cf71dc21bed295d1b8c4
Mohamed Abdulrashid <mohamedabdul9977@gmail.com>
2025-06-08 12:41:58 +0300

    Initial Commit

--- mobileMoneyEstimator/fee.js
+++ mobileMoneyEstimator/fee.js
@@ -1,36 +1,31 @@
 //The Function.
 function estimateTransactionFee() {
-//This part prompts the user to enter the amount of money they want to send.
     const input = prompt(`Unatuma Ngapi? (KES):`);
-//amountToSend makes sure amount entered is a number    
     const amountToSend = Number(input);
-//These are the variables I declared and I also show how to calculate the total amount.    
-    const transactionFee = 1.5;
-    let calculatedTransactionFee = amountToSend * (transactionFee / 100);
-    const totalAmount = amountToSend + calculatedTransactionFee;
+    const transactionFeePercent = 1.5;
+    
+// Calculate initial fee
+    let calculatedTransactionFee = amountToSend * (transactionFeePercent / 100);
+    
 //The first if statement to make sure minimum transaction fee is 10.
     if (calculatedTransactionFee < 10) {
-        let calculatedTransactionFee = 10;
-        console.log(`
-        Sending KES ${amountToSend}:
-        Calculated Transaction Fee: KES ${calculatedTransactionFee}
-        Total amount to be debited: KES ${amountToSend + calculatedTransactionFee}
-
-        Send Money Securely!
-        `);
-//The second statement to make sure maximum transaction fee is 70.        
+        calculatedTransactionFee = 10;
+//The second statement to make sure maximum transaction fee is 70. 
     } else if (calculatedTransactionFee > 70) {
-        let calculatedTransactionFee = 70;
-        console.log(`
+        calculatedTransactionFee = 70;
+    }
+    
+    const totalAmount = amountToSend + calculatedTransactionFee;
+
+    console.log(`
         Sending KES ${amountToSend}:
         Calculated Transaction Fee: KES ${calculatedTransactionFee}
-        Total amount to be debited: KES ${amountToSend + calculatedTransactionFee}
+        Total amount to be debited: KES ${totalAmount}
 
         Send Money Securely!
-        `);
-    }
+    `);
 //The return for the function.
-        return amountToSend;
+    return amountToSend;
 }
 //This is where I invoke the function.
 estimateTransactionFee();
